9d921ce42716db1729c125db67882eea62e5e463
BeneDev <seriousbenedikt@web.de>
2018-06-07 15:41:16 +0200

    Initial commit

---
+++ Assets/PrefabSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour {
+
+    public GameObject prefab;
+    public int spawnCount = 1;
+    public Vector3 rotation;
+    Quaternion objectRotation;
+    public Vector3 offset;
+    public bool isOffsetRelative = false;
+    public bool deleteObjectsFromBefore = false;
+
+    public void SpawnObjects(GameObject prefab, int count, Vector3 offset)
+    {
+        objectRotation = Quaternion.Euler(rotation);
+        if(deleteObjectsFromBefore)
+        {
+            for (int i = transform.childCount -1; i >= 0; i--)
+            {
+                DestroyImmediate(transform.GetChild(i).gameObject);
+            }
+        }
+        if(!isOffsetRelative)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Instantiate(prefab, offset * i, objectRotation, transform);
+            }
+        }
+        else
+        {
+            Transform lastTransform = transform;
+            for (int i = 0; i < count; i++)
+            {
+                GameObject lastSpawned = Instantiate(prefab, lastTransform.position, objectRotation * lastTransform.rotation, transform);
+                lastSpawned.transform.position  += new Vector3(offset.x * lastTransform.right.x, offset.y * lastTransform.right.y, offset.z * lastTransform.right.z)
+                                                + new Vector3(offset.x * lastTransform.up.x, offset.y * lastTransform.up.y, offset.z * lastTransform.up.z) 
+                                                + new Vector3(offset.x * lastTransform.forward.x, offset.y * lastTransform.forward.y, offset.z * lastTransform.forward.z);
+                lastTransform = lastSpawned.transform;
+            }
+        }
+    }
+}
---
+++ Assets/PrefabSpawnerCustomInspector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PrefabSpawner))]
+public class PrefabSpawnerCustomInspector : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        PrefabSpawner script = (PrefabSpawner)target;
+
+        base.OnInspectorGUI();
+
+        //script.prefab = (GameObject)EditorGUILayout.ObjectField(script.prefab, typeof(GameObject));
+        //script.spawnCount = EditorGUILayout.IntField("Spawn Count", script.spawnCount);
+        //script.relativeOffset = EditorGUILayout.Vector3Field("Relative Offset", script.relativeOffset);
+
+        if (GUILayout.Button("Spawn"))
+        {
+            script.SpawnObjects(script.prefab, script.spawnCount, script.offset);
+        }
+    }
+
+}
